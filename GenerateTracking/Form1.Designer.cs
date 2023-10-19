6569458e917a84e2282916cb75c06084a9f934d8
hungnv20797 <140627017+hungnv20797@users.noreply.github.com>
2023-10-19 22:55:24 +0700

    push

---
+++ GenerateTracking/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace GenerateTracking
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExportYO = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExportYO
+            // 
+            this.btnExportYO.Location = new System.Drawing.Point(168, 73);
+            this.btnExportYO.Name = "btnExportYO";
+            this.btnExportYO.Size = new System.Drawing.Size(213, 69);
+            this.btnExportYO.TabIndex = 0;
+            this.btnExportYO.Text = "Export YO";
+            this.btnExportYO.UseVisualStyleBackColor = true;
+            this.btnExportYO.Click += new System.EventHandler(this.btnExportYO_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(592, 73);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(200, 69);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1448, 797);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnExportYO);
+            this.Name = "Form1";
+            this.Text = "Generate Tracking";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExportYO;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ GenerateTracking/Form1.cs
@@ -0,0 +1,301 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Util.Store;
+using Google.Apis.Services;
+
+namespace GenerateTracking
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void GenerateTracking()
+        {
+            string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
+            string ApplicationName = "Your Application Name";
+            string SpreadsheetId = "1WlPa_ANWw3pf96Gbc_vIVUoNdwHjl13JdSEhiESpcZA";
+            string CredentialsFilePath = "./client_secret.json";
+
+            string filePath = "time.txt";
+
+            string OutputCsvFilePath = "ex.csv";
+            UserCredential credential;
+
+            using (var stream = new FileStream("gg.json", FileMode.Open, FileAccess.Read))
+            {
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore("client_secret.json", true)).Result;
+            }
+
+            var service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            string range = "sheet1!A5000:E1000000";
+
+            SpreadsheetsResource.ValuesResource.GetRequest request =
+                service.Spreadsheets.Values.Get(SpreadsheetId, range);
+            ValueRange response = request.Execute();
+            IList<IList<object>> values = response.Values;
+
+            if (values != null && values.Count > 0)
+            {
+
+                var csvContent_SA = new StringBuilder();
+                var csvContent_SZ = new StringBuilder();
+                var csvContent_AG = new StringBuilder();
+                var csvContent_AS = new StringBuilder();
+                var csvContent_AP = new StringBuilder();
+                var csvContent_FL = new StringBuilder();
+                string header = "Order ID,Order Item ID,Carrier Slug,Tracking Number";
+                csvContent_SA.AppendLine(header);
+                csvContent_SZ.AppendLine(header);
+                csvContent_AG.AppendLine(header);
+                csvContent_AS.AppendLine(header);
+                csvContent_AP.AppendLine(header);
+                csvContent_FL.AppendLine(header);
+
+                bool[] checks = new bool[] { false, false, false, false, false, false };
+
+                string startDate = File.ReadAllText(filePath);
+                DateTime currentDate = DateTime.Now.AddDays(-6);
+                int month = currentDate.Month;
+                int day = currentDate.Day;
+                string endDate = month.ToString() + day.ToString();
+                foreach (var row in values)
+                {
+
+                    do { row.Add(""); } while (row.Count < 5);
+                    string time = row[0].ToString();
+                    if (string.Compare(time, startDate) > 0 && string.Compare(time, endDate) < 0)
+                    {
+                        string orderNumber = row[3].ToString();
+                        string logisticTracking = row[4].ToString();
+                        string carrier = getCarrier(logisticTracking);
+
+                        string orderNumberAfter = orderNumber.Length > 2 ? orderNumber.Substring(2) : "0_0";
+                        string shopName = orderNumber.Length > 2 ? orderNumber.Substring(0, 2) : "00";
+
+                        var order = orderNumberAfter.Split('_');
+                        string textExport = order[0] + "," + order[1] + "," + carrier + "," + logisticTracking;
+                        if (shopName == "SA")
+                        {
+                            checks[0] = true;
+                            csvContent_SA.AppendLine(textExport);
+                        }
+                        else if (shopName == "SZ")
+                        {
+                            checks[1] = true;
+                            csvContent_SZ.AppendLine(textExport);
+                        }
+                        else if (shopName == "AG")
+                        {
+                            checks[2] = true;
+                            csvContent_AG.AppendLine(textExport);
+                        }
+                        else if (shopName == "AS")
+                        {
+                            checks[3] = true;
+                            csvContent_AS.AppendLine(textExport);
+                        }
+                        else if (shopName == "AP")
+                        {
+                            checks[4] = true;
+                            csvContent_AP.AppendLine(textExport);
+                        }
+                        else if (shopName == "FL")
+                        {
+                            checks[5] = true;
+                            csvContent_FL.AppendLine(textExport);
+                        }
+                    }
+                }
+                if (checks[0] == true)
+                {
+                    File.WriteAllText("tracking_SA.csv", csvContent_SA.ToString());
+                }
+                if (checks[1] == true)
+                {
+                    File.WriteAllText("tracking_SZ.csv", csvContent_SZ.ToString());
+                }
+                if (checks[2] == true)
+                {
+                    File.WriteAllText("tracking_AG.csv", csvContent_AG.ToString());
+                }
+                if (checks[3] == true)
+                {
+                    File.WriteAllText("tracking_AS.csv", csvContent_AS.ToString());
+                }
+                if (checks[4] == true)
+                {
+                    File.WriteAllText("tracking_AP.csv", csvContent_AP.ToString());
+                }
+                if (checks[5] == true)
+                {
+                    File.WriteAllText("tracking_FL.csv", csvContent_FL.ToString());
+                }
+
+                File.WriteAllText(filePath, endDate);
+
+                MessageBox.Show("Export Success");
+            }
+            else
+            {
+                MessageBox.Show("No data");
+            }
+        }
+        private void btnExportYO_Click(object sender, EventArgs e)
+        {
+            GenerateTracking();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public string getCarrier(string trackingNumber)
+        {
+            var carrierSlug = "";
+            if (trackingNumber.StartsWith("YT"))
+            {
+                carrierSlug = "yun-express-cn";
+            }
+            else if (trackingNumber.StartsWith("007") && trackingNumber.Length == 20)
+            {
+                carrierSlug = "new-zealand-post";
+            }
+            else if (trackingNumber.StartsWith("YDH"))
+            {
+                carrierSlug = "ydh";
+            }
+            else if (trackingNumber.StartsWith("JPS"))
+            {
+                carrierSlug = "jpsgj";
+            }
+            else if (trackingNumber.StartsWith("JY"))
+            {
+                carrierSlug = "uniuni";
+            }
+            else if (trackingNumber.StartsWith("SF"))
+            {
+                carrierSlug = "sf-express-cn";
+            }
+            else if (trackingNumber.StartsWith("JJD"))
+            {
+                carrierSlug = "yodel";
+            }
+            else if (trackingNumber.StartsWith("DY") || trackingNumber.StartsWith("3A"))
+            {
+                carrierSlug = "cne";
+            }
+            else if (trackingNumber.StartsWith("FJ"))
+            {
+                carrierSlug = "royal-mail";
+            }
+            else if (trackingNumber.StartsWith("H100"))
+            {
+                carrierSlug = "hermes-de";
+            }
+            else if (trackingNumber.StartsWith("LR"))
+            {
+                carrierSlug = "china-post";
+            }
+            else if (trackingNumber.StartsWith("34W") || (trackingNumber.StartsWith("36") && trackingNumber.Length == 23))
+            {
+                carrierSlug = "ubi";
+            }
+            else if (trackingNumber.StartsWith("33XH"))
+            {
+                carrierSlug = "australia-post";
+            }
+            else if (trackingNumber.StartsWith("33XJ"))
+            {
+                carrierSlug = "australia-post";
+            }
+            else if (trackingNumber.StartsWith("YHT"))
+            {
+                carrierSlug = "yht";
+            }
+            else if (trackingNumber.StartsWith("JC"))
+            {
+                carrierSlug = "jcex";
+            }
+            else if (trackingNumber.StartsWith("LY"))
+            {
+                carrierSlug = "china-post";
+            }
+            else if (trackingNumber.StartsWith("UG") || trackingNumber.StartsWith("UH"))
+            {
+                carrierSlug = "yanwen";
+            }
+            else if (trackingNumber.StartsWith("LV"))
+            {
+                carrierSlug = "china-post";
+            }
+            else if (trackingNumber.StartsWith("LH"))
+            {
+                carrierSlug = "china-post";
+            }
+            else if (trackingNumber.StartsWith("4PX"))
+            {
+                carrierSlug = "4px";
+            }
+            else if (trackingNumber.StartsWith("1Z"))
+            {
+                carrierSlug = "ups";
+            }
+            else if (trackingNumber.StartsWith("US"))
+            {
+                carrierSlug = "jt-international";
+            }
+            else if (trackingNumber.Length == 10)
+            {
+                carrierSlug = "dhl";
+            }
+            else if (trackingNumber.Length == 12)
+            {
+                carrierSlug = "fedex";
+            }
+            else if (trackingNumber.Length == 16)
+            {
+                carrierSlug = "canada-post";
+            }
+            else if (trackingNumber.Length >= 26)
+            {
+                carrierSlug = "usps";
+            }
+            else if (trackingNumber.Split('|').Length > 1)
+            {
+                carrierSlug = trackingNumber.Split('|')[1];
+            }
+            else
+            {
+                carrierSlug = "NOTFOUND";
+            }
+            return carrierSlug;
+        }
+    }
+}
